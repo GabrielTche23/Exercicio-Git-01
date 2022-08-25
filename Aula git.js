3ba0245847b21f34b3a5e9bcf5736492939c53cf
Gabriel Rodrigo <pdktche-pb@live.com>
2022-08-25 00:19:05 -0300

    Adicionando um Array de todos os valores que o usario inseriu e uma mensagem exibindo todos os valores que o usario inseriu e mostranndo tambem o cpf do usario

--- Aula git.js
+++ Aula git.js
@@ -5,11 +5,15 @@ var continuar = true
 var maiorValor = 0;
 var valorTotal = 0;
 var quantidadedeValor = 0;
+var contador = 0;
+var valoresInseridos = []
 while(continuar){
-    var opcao = prompt("Digite 1 para Saque e 2 para Sacar: ")
+    var opcao = prompt("Digite 1 para Saque e 2 para Depositar: ")
     var valor = parseFloat(prompt("Digite um valor: "))
     valorTotal = valorTotal + valor
     quantidadedeValor++
+    valoresInseridos[contador] = valor
+    contador++
     if(maiorValor <= valor){
         maiorValor = valor
     }
@@ -19,12 +23,12 @@ while(continuar){
     else if (opcao == 2 ){
         saldoTotal = saldoTotal + valor
     }
-    var desejaContinuar = prompt("Deseja continuar? Se Sim digite 1 se caso deseja encerrar Digite 2")
-    if(desejaContinuar = 2){
+    var desejaContinuar = prompt("Deseja continuar? Se Sim digite 1 se caso deseja encerrar Digite 2:")
+    if(desejaContinuar == 2){
         continuar = false
     }
-    
 }
 console.log ("O saldo total foi: ", saldoTotal)
 console.log("O maior valor inserido foi: ", maiorValor)
 console.log("A média foi: ", valorTotal/quantidadedeValor)
+console.log("Todos os valores inseridos foram: ",valoresInseridos , "do cpf:",cpf)
